567f0ff8ba6185d5cfe16a94224e0d7dd853e4bb
tomaslinkina <altoma@mail.ru>
2022-05-29 17:43:09 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int TicketPrice = 405;
+        int MilePrice = 20;
+        int mile = TicketPrice / MilePrice;
+        System.out.println(mile);
+    }
+}
+
